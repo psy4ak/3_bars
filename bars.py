4da34f4fcfcd440899181db92fd7e7ec8f935691
psy4ak <psy4ak@gmail.com>
2016-12-11 20:37:02 +0300

    Update bars.py

--- bars.py
+++ bars.py
@@ -2,20 +2,38 @@ import json
 
 
 def load_data(filepath):
-    pass
+    with open(filepath) as loads:
+        data = json.load(loads)
+        return data
 
 
 def get_biggest_bar(data):
-    pass
+    big = sorted(data, key=lambda k: k['SeatsCount'], reverse=True)
+    return big[0]['Name']
 
 
 def get_smallest_bar(data):
-    pass
+    small = sorted(data, key=lambda k: k['SeatsCount'])
+    return small[0]['Name']
 
 
 def get_closest_bar(data, longitude, latitude):
-    pass
+    for bar in data:
+        #closer distance: sqrt((x2-x1)^2+(y2-y1)^2)
+        distance =(
+        ((float(bar['Longitude_WGS84'])-longitude)**2+
+        (float(bar['Latitude_WGS84'])-latitude)**2)**0.5
+        )
+        #add to bars data distance_key. Lower key - closest
+        bar['distance_key']=distance
+    closest = sorted(data, key=lambda k: k['distance_key'])
+    return closest[0] ['Name']
 
 
 if __name__ == '__main__':
-    pass
+    
+    data=load_data('databars.json')
+    big=get_biggest_bar(data)
+    small=get_smallest_bar(data)
+    closest=get_closest_bar(data, 37.5830439469214500, 55.7578216589971)
+    print ('Наибольший ',big,'\nНаименьший ',small,'\nБлижайший ',closest)
