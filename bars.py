e7221f2ae164664028357e282ba9e290d7372960
psy4ak <psy4ak@gmail.com>
2016-12-16 21:16:50 +0300

    corrected_v3
    - corrected code according to comments
    - style fix according to pep8
    - removed unnecessary files

--- bars.py
+++ bars.py
@@ -1,38 +1,41 @@
 import json
 
+
 def load_data(filepath):
     with open(filepath) as databars_loads:
         bars_data = json.load(databars_loads)
         return bars_data
 
+
 def get_biggest_bar(data):
-    big_bar = sorted(data, key=lambda k: k['SeatsCount'], reverse=True)
-    return big_bar[0]['Name']
+    big_bar = max(data, key=lambda k: k['SeatsCount'])
+    return big_bar['Name']
 
 
 def get_smallest_bar(data):
-    small_bar = sorted(data, key=lambda k: k['SeatsCount'])
-    return small_bar[0]['Name']
+    small_bar = min(data, key=lambda k: k['SeatsCount'])
+    return small_bar['Name']
 
 
 def get_closest_bar(data, longitude, latitude):
     for bar in data:
-        #closer distance: sqrt((x2-x1)^2+(y2-y1)^2)
-        distance =(
-        ((float(bar['Longitude_WGS84'])-longitude)**2+
-        (float(bar['Latitude_WGS84'])-latitude)**2)**0.5
-        )
-        #add to bars data distance_key. Lower key - closest
-        bar['distance_key']=distance
-    closest_bar = sorted(data, key=lambda k: k['distance_key'])
-    return closest_bar[0] ['Name']
+        # closer distance: sqrt((x2-x1)^2+(y2-y1)^2)
+        distance = (
+                    ((float(bar['Longitude_WGS84'])-longitude)**2 +
+                     (float(bar['Latitude_WGS84'])-latitude)**2)**0.5)
+        # add to bars data distance_key. Lower key - closest
+        bar['distance_key'] = distance
+    closest_bar = min(data, key=lambda k: k['distance_key'])
+    return closest_bar['Name']
 
 if __name__ == '__main__':
-    
-    load_data=load_data('databars.json')
-    big=get_biggest_bar(load_data)
-    small=get_smallest_bar(load_data)
-    latitude = float(input('Широта '))
-    longitude = float(input('Долгота '))
-    closest=get_closest_bar(load_data, longitude, latitude)
-    print ('Наибольший бар ',big,'\nНаименьший бар ',small,'\nБлижайший бар',closest)
+    file_path = input('Укажите путь к файлу списка баров (json): ')
+    bars_data = load_data(file_path)
+    big_bar = get_biggest_bar(bars_data)
+    small_bar = get_smallest_bar(bars_data)
+    my_latitude = float(input('Широта '))
+    my_longitude = float(input('Долгота '))
+    closest_bar = get_closest_bar(bars_data, my_longitude, my_latitude)
+    print(
+        'Наибольший бар: ', big_bar, '\nНаименьший бар: ', small_bar,
+        '\nБлижайший бар: ', closest_bar)
